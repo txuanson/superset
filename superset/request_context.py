4eed2889f8e2997191d4e3291eb3ea3740812967
Daniel Gaspar <danielvazgaspar@gmail.com>
2020-05-19 10:32:25 +0100

    nit

--- superset/request_context.py
+++ superset/request_context.py
@@ -14,11 +14,10 @@ def before_request():
     g.tenant_id = subdomain
 
 
-def tenant_switch(some_engine):
-    @event.listens_for(some_engine, "before_cursor_execute", retval=True)
+def tenant_switch(metadata_engine):
+    @event.listens_for(metadata_engine, "before_cursor_execute", retval=True)
     def before_cursor_switch(conn, cursor, stmt,
                         params, context, executemany):
-        # Avoid infinite recursion
         if not hasattr(g, "tenant_id"):
             return stmt, params
         if g.tenant_id is not None:
