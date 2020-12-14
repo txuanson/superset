85bce3dfcacc787fb12dbcb24bf6bffdfd33d6c2
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2020-12-14 15:37:27 -0800

    more updates

--- superset-frontend/src/explore/components/controls/DatasourceControl.jsx
+++ superset-frontend/src/explore/components/controls/DatasourceControl.jsx
@@ -69,7 +69,7 @@ const Styles = styled.div`
     cursor: pointer;
   }
   .title-select {
-    width: ${({ theme }) => theme.gridUnit * 46}px;
+    width: ${({ theme }) => theme.gridUnit * 49}px;
     display: inline-block;
     background-color: #f0f0f0;
     padding: ${({ theme }) => theme.gridUnit * 2}px;
