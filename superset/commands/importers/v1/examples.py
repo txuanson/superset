9d4ac1c1fb8e3bdd82c97866c2cc42b88ae0db2d
Beto Dealmeida <roberto@dealmeida.net>
2021-08-10 16:30:19 -0700

    Small fixes

--- superset/commands/importers/v1/examples.py
+++ superset/commands/importers/v1/examples.py
@@ -14,8 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+# pylint: disable=protected-access
 
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Set, Tuple
 
 from marshmallow import Schema
 from sqlalchemy.orm import Session
@@ -23,23 +24,28 @@ from sqlalchemy.orm.exc import MultipleResultsFound
 from sqlalchemy.sql import select
 
 from superset import db
+from superset.charts.commands.importers.v1 import ImportChartsCommand
 from superset.charts.commands.importers.v1.utils import import_chart
 from superset.charts.schemas import ImportV1ChartSchema
 from superset.commands.exceptions import CommandException
 from superset.commands.importers.v1 import ImportModelsCommand
 from superset.dao.base import BaseDAO
+from superset.dashboards.commands.importers.v1 import ImportDashboardsCommand
 from superset.dashboards.commands.importers.v1.utils import (
     find_chart_uuids,
     import_dashboard,
     update_id_refs,
 )
 from superset.dashboards.schemas import ImportV1DashboardSchema
+from superset.databases.commands.importers.v1 import ImportDatabasesCommand
 from superset.databases.commands.importers.v1.utils import import_database
 from superset.databases.schemas import ImportV1DatabaseSchema
+from superset.datasets.commands.importers.v1 import ImportDatasetsCommand
 from superset.datasets.commands.importers.v1.utils import import_dataset
 from superset.datasets.schemas import ImportV1DatasetSchema
 from superset.models.core import Database
 from superset.models.dashboard import dashboard_slices
+from superset.utils.core import get_example_database
 
 
 class ImportExamplesCommand(ImportModelsCommand):
@@ -71,6 +77,15 @@ class ImportExamplesCommand(ImportModelsCommand):
             db.session.rollback()
             raise self.import_error()
 
+    @classmethod
+    def _get_uuids(cls) -> Set[str]:
+        return (
+            ImportDatabasesCommand._get_uuids()
+            | ImportDatasetsCommand._get_uuids()
+            | ImportChartsCommand._get_uuids()
+            | ImportDashboardsCommand._get_uuids()
+        )
+
     # pylint: disable=too-many-locals, arguments-differ, too-many-branches
     @staticmethod
     def _import(
@@ -92,14 +107,12 @@ class ImportExamplesCommand(ImportModelsCommand):
         # We need to determine its ID so we can point the dataset to it.
         examples_db = (
             db.session.query(Database).filter_by(database_name="examples").first()
-        )
+        ) or get_example_database()
         dataset_info: Dict[str, Dict[str, Any]] = {}
         for file_name, config in configs.items():
             if file_name.startswith("datasets/"):
                 # find the ID of the corresponding database
                 if config["database_uuid"] not in database_ids:
-                    if examples_db is None:
-                        raise Exception("Cannot find examples database")
                     config["database_id"] = examples_db.id
                 else:
                     config["database_id"] = database_ids[config["database_uuid"]]
