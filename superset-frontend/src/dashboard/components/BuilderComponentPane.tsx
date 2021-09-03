a8513720bc835183850552c37337df158a92b2e3
Evan Rusackas <evan@preset.io>
2021-09-02 21:59:49 -0600

    Spoofed Dashboard Properties Editor

--- superset-frontend/src/dashboard/components/BuilderComponentPane.tsx
+++ superset-frontend/src/dashboard/components/BuilderComponentPane.tsx
@@ -22,8 +22,14 @@ import Tabs from 'src/components/Tabs';
 import { StickyContainer, Sticky } from 'react-sticky';
 import { ParentSize } from '@vx/responsive';
 
-import { t, styled } from '@superset-ui/core';
+import { t, styled, css } from '@superset-ui/core';
 
+import { JsonEditor } from 'src/components/AsyncAceEditor';
+import ColorSchemeControlWrapper from 'src/dashboard/components/ColorSchemeControlWrapper';
+import { Select } from 'src/components';
+import { Form, FormItem } from 'src/components/Form';
+import { Input } from 'src/common/components';
+import Button from 'src/components/Button';
 import NewColumn from './gridComponents/new/NewColumn';
 import NewDivider from './gridComponents/new/NewDivider';
 import NewHeader from './gridComponents/new/NewHeader';
@@ -44,10 +50,11 @@ const SUPERSET_HEADER_HEIGHT = 59;
 
 const GeneralTabs = styled(Tabs)`
   line-height: inherit;
-  >.ant-tabs-nav {
+  > .ant-tabs-nav {
     margin: 0;
     width: fit-content;
   }
+  height: 100%;
 `;
 const SpecificTabs = styled(Tabs)`
   line-height: inherit;
@@ -55,7 +62,108 @@ const SpecificTabs = styled(Tabs)`
   padding: 8px;
 `;
 
-const BuilderComponentPane: React.FC<BCPProps> = ({ topOffset = 0, updateCss, onChange, customCss }) => (
+const StyledJsonEditor = styled(JsonEditor)`
+  border-radius: ${({ theme }) => theme.borderRadius}px;
+  border: 1px solid ${({ theme }) => theme.colors.secondary.light2};
+`;
+
+const BSProperties: React.FC<> = () => (
+  <>
+    {/* SMOKE AND MIRRORS */}
+    <Form
+      data-test="dashboard-edit-properties-form"
+      // onSubmit={this.submit}
+      layout="vertical"
+    >
+      <h3>{t('Basic information')}</h3>
+      <FormItem label={t('Title')}>
+        <Input
+          data-test="dashboard-title-input"
+          name="dashboard_title"
+          type="text"
+          value="Dashboard Title"
+          // onChange={this.onChange}
+          disabled={false}
+        />
+      </FormItem>
+      <FormItem label={t('URL slug')}>
+        <Input
+          name="slug"
+          type="text"
+          value="Dashboard slug"
+          // onChange={this.onChange}
+          disabled={false}
+        />
+        <p className="help-block">{t('A readable URL for your dashboard')}</p>
+      </FormItem>
+      <h3 style={{ marginTop: '1em' }}>{t('Access')}</h3>
+      <FormItem label={t('Owners')}>
+        <Select
+          allowClear
+          ariaLabel={t('Owners')}
+          disabled={false}
+          name="owners"
+          mode="multiple"
+          value={[]}
+          options={() => new Promise(() => [])}
+          // onChange={this.onOwnersChange}
+        />
+        <p className="help-block">
+          {t(
+            'Owners is a list of users who can alter the dashboard. Searchable by name or username.',
+          )}
+        </p>
+      </FormItem>
+      <h3 style={{ marginTop: '1em' }}>{t('Colors')}</h3>
+      <ColorSchemeControlWrapper
+        // onChange={this.onColorSchemeChange}
+        colorScheme="supersetColors" // TODO Spoof this!
+        labelMargin={4}
+      />
+      <h3 style={{ marginTop: '1em' }}>
+        <Button
+          buttonStyle="link"
+          // onClick={this.toggleAdvanced}
+        >
+          <i
+            className={`fa fa-angle-${
+              // isAdvancedOpen ? 'down' : 'right'
+              'down'
+            }`}
+            style={{ minWidth: '1em' }}
+          />
+          {t('Advanced')}
+        </Button>
+      </h3>
+      <FormItem label={t('JSON metadata')}>
+        <StyledJsonEditor
+          showLoadingForImport
+          name="json_metadata"
+          // defaultValue={this.defaultMetadataValue}
+          // value={values.json_metadata}
+          // onChange={this.onMetadataChange}
+          tabSize={2}
+          width="100%"
+          height="200px"
+          wrapEnabled
+        />
+        <p className="help-block">
+          {t(
+            'This JSON object is generated dynamically when clicking the save or overwrite button in the dashboard view. It is exposed here for reference and for power users who may want to alter specific parameters.',
+          )}
+        </p>
+      </FormItem>
+    </Form>
+    {/* END SMOKE AND MIRRORS */}
+  </>
+);
+
+const BuilderComponentPane: React.FC<BCPProps> = ({
+  topOffset = 0,
+  updateCss,
+  onChange,
+  customCss,
+}) => (
   <div
     className="dashboard-builder-sidepane"
     style={{
@@ -71,60 +179,65 @@ const BuilderComponentPane: React.FC<BCPProps> = ({ topOffset = 0, updateCss, on
                 className="viewport"
                 style={isSticky ? { ...style, top: topOffset } : null}
               >
-              <h5 style={{
-                margin: "12px 6px",
-                fontSize: "14px",
-              }}>{t('Edit Dashboard')}</h5>
-              <GeneralTabs type="card">
-                <Tabs.TabPane key={1} tab={t('Content')}>
-                  <SpecificTabs
-                    id="tabs"
-                    className="tabs-components"
-                    data-test="dashboard-builder-component-pane-tabs-navigation"
-                  >
-                    <Tabs.TabPane key={1} tab={t('Components')}>
-                      <NewTabs />
-                      <NewRow />
-                      <NewColumn />
-                      <NewHeader />
-                      <NewMarkdown />
-                      <NewDivider />
-                    </Tabs.TabPane>
-                    <Tabs.TabPane
-                      key={2}
-                      tab={t('Charts')}
-                      className="tab-charts"
+                <h5
+                  style={{
+                    margin: '12px 6px',
+                    fontSize: '14px',
+                  }}
+                >
+                  {t('Edit Dashboard')}
+                </h5>
+                <GeneralTabs type="card">
+                  <Tabs.TabPane key={1} tab={t('Content')}>
+                    <SpecificTabs
+                      id="tabs"
+                      className="tabs-components"
+                      data-test="dashboard-builder-component-pane-tabs-navigation"
                     >
-                      <SliceAdder
-                        height={height + (isSticky ? SUPERSET_HEADER_HEIGHT : 0)}
-                      />
-                    </Tabs.TabPane>
-                  </SpecificTabs>
-                </Tabs.TabPane>
-                <Tabs.TabPane key={2} tab={t('Customize')}>
-                  <SpecificTabs>
-                    <Tabs.TabPane
-                      key={1}
-                      tab={t('Properties')}
-                    >
-                      {"'Edit dashboard properties' modal content here"}
-                    </Tabs.TabPane>
-                    <Tabs.TabPane
-                      key={2}
-                      tab={t('CSS')}
-                    >
-                      <CssEditor
-                        triggerNode={<span>{t('Edit CSS')}</span>}
-                        initialCss={customCss}
-                        onChange={onChange}
-                        updateCss={updateCss}
-                      />
-                    </Tabs.TabPane>
-
-                  </SpecificTabs>
-                </Tabs.TabPane>
-
-              </GeneralTabs>
+                      <Tabs.TabPane key={1} tab={t('Components')}>
+                        <NewTabs />
+                        <NewRow />
+                        <NewColumn />
+                        <NewHeader />
+                        <NewMarkdown />
+                        <NewDivider />
+                      </Tabs.TabPane>
+                      <Tabs.TabPane
+                        key={2}
+                        tab={t('Charts')}
+                        className="tab-charts"
+                      >
+                        <SliceAdder
+                          height={
+                            height + (isSticky ? SUPERSET_HEADER_HEIGHT : 0)
+                          }
+                        />
+                      </Tabs.TabPane>
+                    </SpecificTabs>
+                  </Tabs.TabPane>
+                  <Tabs.TabPane key={2} tab={t('Customize')}>
+                    <SpecificTabs>
+                      <Tabs.TabPane key={1} tab={t('Properties')}>
+                        <BSProperties />
+                      </Tabs.TabPane>
+                      <Tabs.TabPane key={2} tab={t('CSS')}>
+                        <CssEditor
+                          triggerNode={<span>{t('Edit CSS')}</span>}
+                          initialCss={customCss}
+                          onChange={onChange}
+                          updateCss={updateCss}
+                          css={css`
+                            position: absolute;
+                            top: 0;
+                            right: 0;
+                            bottom: 0;
+                            left: 0;
+                          `}
+                        />
+                      </Tabs.TabPane>
+                    </SpecificTabs>
+                  </Tabs.TabPane>
+                </GeneralTabs>
               </div>
             )}
           </Sticky>
